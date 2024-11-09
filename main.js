616b7d51ee631a4fdf0353ce01f166fbbfb8a808
Ali Hassan <alehassaan@gmail.com>
2024-11-09 02:49:29 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menuIcon = document.querySelector(".menu-icon");
+
+    menuIcon.addEventListener("click", function() {
+        menuIcon.classList.toggle("move");
+    });
+});
